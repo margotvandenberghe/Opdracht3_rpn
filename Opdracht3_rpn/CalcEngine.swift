9c2302d19e28c93bf113bf15375182635e820145
Margot Vandenberghe <margotvandenberghe@margots-imac.lan>
2018-10-01 07:25:27 -0700

    First

---
+++ Opdracht3_rpn/CalcEngine.swift
@@ -0,0 +1,18 @@
+//
+//  CalcEngine.swift
+//  Opdracht3_rpn
+//
+//  Created by Margot Vandenberghe on 01/10/2018.
+//  Copyright © 2018 margotvandenberghe. All rights reserved.
+//
+
+import Foundation
+
+class CalcEngine {
+    
+    func clearStack () {
+        
+    }
+    
+    
+}
--- Opdracht3_rpn/ViewController.swift
+++ Opdracht3_rpn/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textViewScherm: UITextView!
+    
+    @IBOutlet weak var labelZin: UILabel!
+    
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var buttonMin: UIButton!
+    @IBOutlet weak var buttonMaal: UIButton!
+    @IBOutlet weak var buttonDelen: UIButton!
+    
+    @IBOutlet weak var buttonCancel: UIButton!
+    @IBOutlet weak var buttonEnter: UIButton!
+    
+    
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func digitPressed(_ sender: Any) {
+    }
+    
+    @IBAction func enterPressed(_ sender: Any) {
+    }
+    
+    @IBAction func operationPressed(_ sender: Any) {
+    }
+    
+    @IBAction func clearPressed(_ sender: Any) {
+    }
 }
 
